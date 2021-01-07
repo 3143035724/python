31a25f52793f7992b6a7ddb309971083ecd09920
suyixing <suyixing>
2021-01-07 16:36:51 +0800

    fix print

--- PythonProject/LearnProjects/learn/basis/two.py
+++ PythonProject/LearnProjects/learn/basis/two.py
@@ -17,17 +17,17 @@ print('特别注意：python以顺序执行方法')
 
 class two():
     def __init__(self):
-        print('two method，打印顺序为6')
+        print('two method，理论执行顺序为2，实际执行顺序为6')
 
     # 声明变量不需要进行赋值
     counter = 500
     miles = 1000.7
     name = 'syx'
-    print(counter, '打印顺序为2')
-    print(miles, '打印顺序为3')
-    print(name, '打印顺序为4')
+    print(counter, '，理论执行顺序为3，实际执行顺序为2')
+    print(miles, '，理论执行顺序为4，实际执行顺序为3')
+    print(name, '，理论执行顺序为5，实际执行顺序为4')
 
 
 if __name__ == '__main__':
-    print('main init ', '打印顺序为5')
+    print('main init ', '，理论执行顺序为1，打印顺序为5')
     two()
