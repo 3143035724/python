8e229b2427537cd164293e7a42c6643b59475283
suyixing <suyixing>
2021-01-28 14:50:17 +0800

    fix 2021-01-28提交 搜索插入位置

--- LearnJdk15/src/main/java/syx/january2021/The28thDay.java
+++ LearnJdk15/src/main/java/syx/january2021/The28thDay.java
@@ -91,7 +91,30 @@ public class The28thDay {
 
 
     @Test
-    public void test() {
+    public void test2() {
         pivotIndex(new int[]{1, 7, 3, 6, 5, 6});
+    }
+
+    /**
+     * 题目：搜索插入位置
+     * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+     * 假设数组中无重复元素。
+     * https://leetcode-cn.com/problems/search-insert-position/submissions/
+     */
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target || nums[i] > target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+
+    @Test
+    public void test3() {
+        searchInsert(new int[]{1, 3, 5, 6}, 5);
     }
 }
