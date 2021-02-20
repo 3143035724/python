3bcf7b649db7d7c1a2daaa204e7bf1c864da6d87
suyixing <suyixing>
2021-02-20 13:55:48 +0800

    fix 2021-02-20 整理

--- LearnJdk15/src/main/java/syx/february2021/The20thDay.java
+++ LearnJdk15/src/main/java/syx/february2021/The20thDay.java
@@ -27,30 +27,32 @@ public class The20thDay {
      * 相关标签：数组、双指针
      * https://leetcode-cn.com/problems/move-zeroes/
      */
+    static class A {
 
-    public void moveZeroes(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return;
-        }
-        // 初始替换位置
-        int count = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                if (count != i) {
-                    // 将count位置值与i位置值进行互换
-                    nums[count] = nums[i];
-                    nums[i] = 0;
+        public void moveZeroes(int[] nums) {
+            if (nums == null || nums.length == 0) {
+                return;
+            }
+            // 初始替换位置
+            int count = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != 0) {
+                    if (count != i) {
+                        // 将count位置值与i位置值进行互换
+                        nums[count] = nums[i];
+                        nums[i] = 0;
+                    }
+                    count++;
                 }
-                count++;
             }
         }
-    }
 
-    @Test
-    public void test1() {
-        int[] nums = {1, 2, 0, 5, 0, 10};
-        moveZeroes(nums);
-        System.out.println(nums);
+        @Test
+        public void test() {
+            int[] nums = {1, 2, 0, 5, 0, 10};
+            moveZeroes(nums);
+            System.out.println(nums);
+        }
     }
 
     /**
@@ -60,61 +62,62 @@ public class The20thDay {
      * 相关标签：数组
      * https://leetcode-cn.com/problems/degree-of-an-array/
      */
-    public int findShortestSubArray(int[] nums) {
-        // key为值，value为出现的次数
-        Map<Integer, Integer> map = new HashMap<>();
-        // 初始化连续出现的频数为0
-        AtomicInteger frequency = new AtomicInteger();
+    static class B {
+        public int findShortestSubArray(int[] nums) {
+            // key为值，value为出现的次数
+            Map<Integer, Integer> map = new HashMap<>();
+            // 初始化连续出现的频数为0
+            AtomicInteger frequency = new AtomicInteger();
 
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                map.replace(nums[i], map.get(nums[i]), map.get(nums[i]) + 1);
-            } else {
-                map.put(nums[i], 1);
-            }
-            frequency.set(Math.max(frequency.get(), map.get(nums[i])));
-        }
-
-        // 获取map中该数量对应的key值
-        Set<Integer> values = getMapKey(map, frequency);
-        // key为值，value为对应下标集合
-        Map<Integer, List<Integer>> mapValues = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            if (values.contains(nums[i])) {
-                // 数量相同的数字集合
-                if (!mapValues.containsKey(nums[i])) {
-                    mapValues.put(nums[i], new ArrayList<>());
+            for (int num : nums) {
+                if (map.containsKey(num)) {
+                    map.replace(num, map.get(num), map.get(num) + 1);
+                } else {
+                    map.put(num, 1);
                 }
-                mapValues.get(nums[i]).add(i);
+                frequency.set(Math.max(frequency.get(), map.get(num)));
             }
+
+            // 获取map中该数量对应的key值
+            Set<Integer> values = getMapKey(map, frequency);
+            // key为值，value为对应下标集合
+            Map<Integer, List<Integer>> mapValues = new HashMap<>();
+
+            for (int i = 0; i < nums.length; i++) {
+                if (values.contains(nums[i])) {
+                    // 数量相同的数字集合
+                    if (!mapValues.containsKey(nums[i])) {
+                        mapValues.put(nums[i], new ArrayList<>());
+                    }
+                    mapValues.get(nums[i]).add(i);
+                }
+            }
+            AtomicInteger minValue = new AtomicInteger();
+            mapValues.forEach((number, frequencyCounts) -> {
+                int min = frequencyCounts.get(0);
+                int max = frequencyCounts.get(frequencyCounts.size() - 1);
+                if (minValue.get() == 0) {
+                    minValue.set(max - min);
+                }
+                minValue.set(Math.min(max - min, minValue.get()));
+            });
+            return minValue.get() + 1;
         }
-        AtomicInteger minValue = new AtomicInteger();
-        mapValues.forEach((number, frequencyCounts) -> {
-            int min = frequencyCounts.get(0);
-            int max = frequencyCounts.get(frequencyCounts.size() - 1);
-            if (minValue.get() == 0) {
-                minValue.set(max - min);
-            }
-            minValue.set(Math.min(max - min, minValue.get()));
-        });
-        return minValue.get() + 1;
-    }
 
-    public Set<Integer> getMapKey(Map<Integer, Integer> map, AtomicInteger frequency) {
-        Set<Integer> set = new HashSet<>();
-        map.forEach((number, count) -> {
-            if (count == frequency.get()) {
-                set.add(number);
-            }
-        });
-        return set;
-    }
+        public Set<Integer> getMapKey(Map<Integer, Integer> map, AtomicInteger frequency) {
+            Set<Integer> set = new HashSet<>();
+            map.forEach((number, count) -> {
+                if (count == frequency.get()) {
+                    set.add(number);
+                }
+            });
+            return set;
+        }
 
-    @Test
-    public void test2() {
-        int[] nums = {1, 2, 2, 3, 1};
-        System.out.println(findShortestSubArray(nums));
+        @Test
+        public void test() {
+            int[] nums = {1, 2, 2, 3, 1};
+            System.out.println(findShortestSubArray(nums));
+        }
     }
-
 }
