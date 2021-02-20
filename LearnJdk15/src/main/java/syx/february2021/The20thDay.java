d0d36ea5332a1d36b4629c9fff1926be7f5b3ef3
suyixing <suyixing>
2021-02-20 18:07:54 +0800

    fix 2021-02-20 环形链表，效率可能不是很高

--- LearnJdk15/src/main/java/syx/february2021/The20thDay.java
+++ LearnJdk15/src/main/java/syx/february2021/The20thDay.java
@@ -209,4 +209,46 @@ public class The20thDay {
         System.out.println(nums1);
     }
 
+    /**
+     * 题目：环形链表
+     * 给定一个链表，如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况
+     * 如果链表中存在环，则返回 true 。 否则，返回 false
+     * 相关标签：链表、双指针
+     * https://leetcode-cn.com/problems/linked-list-cycle/
+     */
+    class D {
+        public boolean hasCycle(ListNode head) {
+            if (head == null || head.next == null) {
+                return false;
+            }
+            ListNode copyHead = head;
+            Set<ListNode> vals = new HashSet<>();
+            while (copyHead != null) {
+                if (!vals.add(copyHead)) {
+                    return true;
+                }
+                copyHead = copyHead.next;
+            }
+            return false;
+        }
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    @Test
+    public void test4() {
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        new D().hasCycle(listNode);
+    }
+
+
 }
