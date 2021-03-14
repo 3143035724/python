e1bcd68c57464536c33189f520af48d9544a8724
suyixing <suyixing>
2021-03-14 17:34:49 +0800

    fix 2021-03-05 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点;最长回文子串

---
+++ LearnJdk15/src/main/java/syx/march2021/The14thDay.java
@@ -0,0 +1,40 @@
+package syx.march2021;
+
+/**
+ * * syx.march2021
+ *
+ * @author syx
+ * @version 1.0.0
+ * @date 2021/3/14 下午 5:29
+ */
+public class The14thDay {
+    /**
+     * 题目：不使用任何内建的哈希表库设计一个哈希集合（HashSet）
+     */
+    class MyHashSet {
+
+        private boolean[] sets = new boolean[1000005];
+
+        /**
+         * Initialize your data structure here.
+         */
+        public MyHashSet() {
+
+        }
+
+        public void add(int key) {
+            sets[key] = true;
+        }
+
+        public void remove(int key) {
+            sets[key] = false;
+        }
+
+        /**
+         * Returns true if this set contains the specified element
+         */
+        public boolean contains(int key) {
+            return sets[key];
+        }
+    }
+}
---
+++ LearnJdk15/src/main/java/syx/march2021/The9thDay.java
@@ -0,0 +1,23 @@
+package syx.march2021;
+
+/**
+ * * syx.march2021
+ *
+ * @author syx
+ * @version 1.0.0
+ * @date 2021/3/9 15:10
+ */
+public class The9thDay {
+
+    public String removeDuplicates(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        StringBuilder split = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (!(i < s.length() - 1 && s.charAt(i) == s.charAt(i + 1))) {
+                split.append(s.charAt(i));
+            }
+        }
+    }
+}
